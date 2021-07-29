f4bd8fed97e9c6e599e73f45cf87393081d5934d
tang <467938742@qq.com>
2021-07-29 16:38:24 +0800

    三种classloader

---
+++ src/com/company/Demo.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Demo {
+    public Demo() {
+        System.out.println("demo instance");
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import sun.net.spi.nameservice.dns.DNSNameService;
+
+import java.io.File;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // BootStrap加载器，C++编写，加载<JAVA_HOME>\jre\lib里面的类库(比如rt.jar，rt是runtime的简称)
+        ClassLoader classLoader = Object.class.getClassLoader();
+        System.out.println(classLoader); // 打印null
+
+        classLoader = Number.class.getClassLoader();
+        System.out.println(classLoader); // 打印null
+
+        //Extension加载器，纯java编写，ExtClassLoader，加载<JAVA_HOME>\jre\lib\ext里面的类库(比如dnsns.jar)或者系统变量"java.ext.dirs"指定目录下面的类
+        ClassLoader classLoader1 = DNSNameService.class.getClassLoader();
+        System.out.println(classLoader1);
+
+
+        //System加载器，纯java编写，AppclassLoader，我们写的java程序一般由这个加载
+        ClassLoader classLoader2 = Main.class.getClassLoader();
+        System.out.println(classLoader2);
+
+//        File file = new File("d:/");
+//        URI uri = file.toURI();
+//        URL url = uri.toURL();
+//
+//        URLClassLoader classLoader3 = new URLClassLoader()
+
+    }
+}
